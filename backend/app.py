640913c01d702cb7e85e4698a9dfd2aedd018e53
sumitkr <sumiitjune@gmail.com>
2026-03-30 21:39:15 +0000

    first push to server monitoring

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+import psutil
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Monitoring Server Running ^) "
+
+@app.route('/stats')
+def stats():
+    cpu = psutil.cpu_percent()
+    memory = psutil.virtual_memory().percent
+    disk = psutil.disk_usage('/').percent
+
+    try:
+        containers = subprocess.check_output("docker ps --format '{{.Names}}'", shell=True).decode().split("\n")
+        containers = [c for c in containers if c]
+    except:
+        containers = []
+
+    return jsonify({
+        "cpu": cpu,
+        "memory": memory,
+        "disk": disk,
+        "containers": containers
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
