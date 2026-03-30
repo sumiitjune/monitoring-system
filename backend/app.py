4ef1d7fa3280df854a768f311457d0df639a2b1b
sumitkr <sumiitjune@gmail.com>
2026-03-30 22:22:03 +0000

    final push with gui

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import psutil
 import subprocess
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/')
 def home():
     return "Monitoring Server Running ^) "
